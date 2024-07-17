7cf0f002bc5762e3a6b34d56fa3b39e601ed0298
VittorBit <victorsupo.262002@gmail.com>
2024-07-16 22:19:26 -0500

    Creacion del proyecto ForoHUB

---
+++ src/test/java/com/proyecto/ChallengeForoHUB_ONE/ChallengeForoHubOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.ChallengeForoHUB_ONE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeForoHubOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
